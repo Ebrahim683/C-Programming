f0fdf69b371182f39194b00af7392afaa2f538d6
Ebrahim <ebrahimrahman20@gmail.com>
2022-05-22 22:52:01 +0600

    initial commit

---
+++ Arithmetic Operators/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+
+   float a,b;
+   a = 105.3;
+   b = 17.3;
+   printf("a = %.2f, b = %.2f\n",a,b);
+   printf("a + b = %.2f\n",a+b);
+   printf("a - b = %.2f\n",a-b);
+   printf("a * b = %.2f\n",a*b);
+   printf("a / b = %.2f\n",a/b);
+
+    return 0;
+}
