5241b0add3d01e83a556f6a659aef83c7310971d
Ebrahim <ebrahimrahman20@gmail.com>
2022-05-23 08:45:15 +0600

    switch case

---
+++ Switch_Case/main.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a;
+
+    printf("Enter a number: ");
+    scanf("%d",&a);
+    switch(a){
+
+case 0:
+    printf("The value of a is %d\n",a);
+    break;
+
+    case 1:
+    printf("The value of a is %d\n",a);
+    break;
+
+    case 2:
+    printf("The value of a is %d\n",a);
+    break;
+
+    case 3:
+    printf("The value of a is %d\n",a);
+    break;
+
+    case 4:
+    printf("The value of a is %d\n",a);
+    break;
+
+    case 5:
+    printf("The value of a is %d\n",a);
+    break;
+
+    deafult:
+    printf("The value of a is out of 5");
+
+    }
+
+    return 0;
+}
