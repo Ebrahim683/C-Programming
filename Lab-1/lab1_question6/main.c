2974f423f01341a52a2ddf8164e0f29f37e1523d
Ebrahim <ebrahimrahman20@gmail.com>
2022-05-28 09:08:00 +0600

    question6

---
+++ Lab-1/lab1_question6/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	int c,d;
+
+	printf("Enter the value of c: ");
+	scanf("%d",&c);
+
+	printf("Enter the value of d: ");
+	scanf("%d",&d);
+
+	printf("c: %d\nd: %d",d,c);
+
+    return 0;
+}
